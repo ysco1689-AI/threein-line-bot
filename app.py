d1cd1a83dc6b09f8b292f33b2634fa03629aa289
ysco1689-AI <ysco1689@gmail.com>
2026-05-10 19:04:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -126,4 +126,5 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
